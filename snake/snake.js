9e680b4e7661b0bab321e75ef7f8bcf6ae0f1c13
blakar <blakar@post.cz>
2015-02-05 23:27:02 -0800

    Paint snake in the upper left corner

--- snake/snake.js
+++ snake/snake.js
@@ -15,13 +15,30 @@ $(document).ready(function () {
     // let's create the snake now
     var snake_array; // an array of cells to make up the snake
 
+    create_snake();
+
     function create_snake() {
         var length = 5; // length of the snake
         snake_array = []; // empty array to start with
 
         for (var i = length - 1; i >= 0; i--) {
             // this will create a horizontal snake starting from the top left
-            snake.array.push({ x: i, y: 0 });
+            snake_array.push({ x: i, y: 0 });
         }
     }
+
+    // let's paint the snake name
+    function paint() {
+        for (var i = 0; i < snake_array.length; i++) {
+            var c = snake_array[i];
+
+            // let's paint 10px wide cells
+            ctx.fillStyle = 'blue';
+            ctx.fillRect(c.x * 10, c.y * 10, 10, 10);
+            ctx.strokeStyle = 'white';
+            ctx.strokeRect(c.x * 10, c.y * 10, 10, 10);
+        }
+    }
+
+    paint();
 });
