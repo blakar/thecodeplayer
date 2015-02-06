74e61205894dbbb57b0e1192858032c4646a2435
blakar <blakar@post.cz>
2015-02-05 23:16:17 -0800

    Add js file

---
+++ snake/snake.js
@@ -0,0 +1,16 @@
+﻿
+$(document).ready(function () {
+    // initialize canvas
+    var canvas = $('#canvas')[0];
+    var ctx = canvas.getContext("2d");
+    var w = $('#canvas').width();
+    var h = $('#canvas').height();
+
+    // let's paint the canvas
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, w, h);
+    ctx.strokeStyle = 'black';
+    ctx.strokeRect(0, 0, w, h);
+
+   
+});
