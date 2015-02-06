ff6ee95020aa33b5fe00f990e1fd082235341e67
blakar <blakar@post.cz>
2015-02-05 23:22:27 -0800

    Initialize the snake

--- snake/snake.js
+++ snake/snake.js
@@ -7,10 +7,21 @@ $(document).ready(function () {
     var h = $('#canvas').height();
 
     // let's paint the canvas
-    ctx.fillStyle = 'white';
+    ctx.fillStyle = 'yellow'; // 'white';
     ctx.fillRect(0, 0, w, h);
     ctx.strokeStyle = 'black';
     ctx.strokeRect(0, 0, w, h);
 
-   
+    // let's create the snake now
+    var snake_array; // an array of cells to make up the snake
+
+    function create_snake() {
+        var length = 5; // length of the snake
+        snake_array = []; // empty array to start with
+
+        for (var i = length - 1; i >= 0; i--) {
+            // this will create a horizontal snake starting from the top left
+            snake.array.push({ x: i, y: 0 });
+        }
+    }
 });
